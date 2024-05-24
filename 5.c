9cba2a8aee7241d5f4bceac590b563cc4fe8828d
Coding-Liu <2836868138@qq.com>
2024-05-24 08:08:47 +0800

    Add files via upload

---
+++ 5.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+// void new_line()
+// {
+//     printf("hehe\n");
+// }
+// void three_line()
+// {
+//     int i = 0;
+//     for(i=0;i<3;i++)
+//     {
+//         new_line();
+//     }
+// }
+// int Add(int x,int y)
+// {
+//     int Sub(int x,int y)
+//     {
+
+//     }
+// }
+// int main()
+// {
+//     three_line();
+//     return 0;
+// }
+// void Add(int x,int y)
+// {
+//     printf("hehe\n");
+// }
+// int main()
+// {
+//     int a=1;
+//     int b=2;
+//     int c=Add(a,b);
+//     printf("%d\n",c);
+//     printf("%d",printf("%d\n",printf("%d\n",43)));
+//     return 0;
+// }
