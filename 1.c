3822d5de07711a439aad6a83502dd9c52c4858bf
Coding-Liu <2836868138@qq.com>
2024-05-25 09:38:47 +0800

    Add files via upload

--- 1.c
+++ 1.c
@@ -1,173 +1,274 @@
 #include <stdio.h>
-// #include <string.h>
-// int main()
+#include <stdlib.h>
+#include <time.h>
+// void game()
 // {
-//     char ch[]={'a','b',0};
-//     printf("%d\n",strlen(ch));
-//     return 0;
-// }
-
-// int main()
-// {
-//     int arr[10]={12,12};
-//     for(int i=0;i<10;i++)
+//     int r=rand()%100+1;
+//     int guess=0;
+//     while(1)
 //     {
-// printf("%d ",arr[i]);
+//         printf("请猜数字：>");
+//         scanf("%d",&guess);
+//         if(guess>r)
+//         {
+//             printf("猜大了！\n");
+//         }
+//         else if(guess<r)
+//         {
+//             printf("猜小了！\n");
+//         }
+//         else
+//         {
+//             printf("恭喜你，猜对了！\n");
+//             break;
+//         }
 //     }
-//     printf("\n");
-//     printf("%d\n",'0');
-//     return 0;
 // }
-
 // int main()
 // {
-//     double a=7.0f/2;
-//     printf("%.2lf\n",a);
+//     srand((unsigned int)time(NULL));
+//     game();
 //     return 0;
 // }
 // int main()
 // {
-//     int x=1;
-//     int y=2;
-//     int z=3;
-//     z=x=y+1;
-//     printf("%d %d\n",x,z);
-//     return 0;
-// }
-// int main()
-// {
-//     int flag=0;
-//     if(!flag)
+//     int n=0;
+//     scanf("%d",&n);
+//     int arr[n];
+//     int i=0;
+//     for(i=0;i<n;i++)
 //     {
-//         printf("hehe\n");
+//      scanf("%d",&arr[i]);
 //     }
-//     else
-//     printf("haha\n");
-//     return 0;
-// }
-// #include <string.h>
-// int main()
-// {
-//     int arr[10]={0,3,3,3,3};
-// printf("%lu\n",strlen(arr));
-// return 0;
-// }
-// int main()
-// {
-//     printf("%lu\n",sizeof(1));
-//     printf("%lu\n",sizeof('a'));
-//     return 0;
-// }
-// int main()
-// {
-//     int i=0;
-//     i++,i++,i++;
-//     int x=0;
-//     int y=0;
-//     x=4,y=3;
-//     printf("%d\n",i);
-//     printf("%d %d\n",x,y);
-
-//     return 0;
-// }
-// typedef struct Stu
-// {
-// char name[10];
-// int age;
-// float weight;
-// }haha;
-// int main()
-// {
-//     haha c={"zhang",18,68.5};
-//     printf("%s %d %.1f\n",c.name,c.age,c.weight);
-//     return 0;
-// }
-// int test(int x)
-// {
-//     static int j=0;
-//     j=j+x;
-//     return j;
-// }
-// int main()
-// {
-//     int r=test(3);
-//     int q=test(4);
-//     printf("%d %d\n",r,q);
-//     return 0;
-// }
-// void test()
-// {
-//     int a=1;
-//     a++;
-//     printf("%d ",a);
-//     printf("%p\n",&a);
-// }
-// int main()
-// {
-//     int i=0;
-//     while(i<10)
+//     int del=0;
+//     scanf("%d",&del);
+//     for(i=0;i<n;i++)
 //     {
-//         test();
-//         i++;
+//         if(arr[i]==del)
+//         continue;
+//         printf("%d ",arr[i]);
 //     }
 //     printf("\n");
 //     return 0;
 // }
 // int main()
 // {
-//     char arr[3]={0};
-//     printf("%p\n",arr+1);
-//     printf("%p\n",arr);
+//     int n=0;
+//     scanf("%d",&n);
+//     int arr[n];
+//     int i=0;
+//     for(i=0;i<n;i++)
+//     {
+//         scanf("%d",&arr[i]);
+//     }
+//     int del=0;
+//     scanf("%d",&del);
+//     int j=0;
+//     for(i=0;i<n;i++)
+//     {
+//         if(arr[i] !=del)
+//         {
+//             arr[j]=arr[i];
+//             j++;
+//         }
+//     }
+//     for(i=0;i<j;i++)
+//     {
+//         printf("%d ",arr[i]);
+//     }
+//     printf("\n");
+//     return 0;
+// }
+// int Max(int arr[],int n)
+// {
+//    int max=arr[0];
+//    int i=0;
+//    for(i=1;i<n;i++)
+//    {
+//     if(arr[i]>max)
+//     {
+//         max=arr[i];
+//     }
+//    }
+//    return max;
+// }
+// int Min(int arr[],int n)
+// {
+//     int min=arr[0];
+//     int i=0;
+//     for(i=1;i<n;i++)
+//     {
+//         if(arr[i]<min)
+//         {
+//             min=arr[i];
+//         }
+//     }
+//     return min;
+// }
+// int main()
+// {
+//     int n=0;
+//     scanf("%d",&n);
+//     int arr[n];
+//     int i=0;
+//     for(i=0;i<n;i++)
+//     {
+//         scanf("%d",&arr[i]);
+//     }
+//     int ret=Max(arr,n)-Min(arr,n);
+//     printf("%d\n",ret);
 //     return 0;
 // }
 // int main()
 // {
-//     int a=10;
-//     int *p=&a;
-//     printf("%p\n",p);
-//     int b=1;
-//     p=&b;
-//     printf("%p\n",p);
-//     char ch='w';
-//     p=&ch;
-//     printf("%p\n",p);
+//     int n=0;
+//     scanf("%d",&n);
+//     int arr[n];
+//     int i=0;
+//     int max=0;
+//     int min=100;
+//     for(i=0;i<n;i++)
+//     {
+//       scanf("%d",&arr[i]);
+//       if(arr[i]>max)
+//       max=arr[i];
+//       if(arr[i]<min)
+//       min=arr[i];
+//     }
+//     printf("%d\n",max-min);
 //     return 0;
 // }
 // int main()
 // {
-//     printf("%d\n",sizeof(int*));
-//     printf("%d\n",sizeof(char*));
-//     printf("%d\n",sizeof(float*));
-//     printf("%d\n",sizeof(double*));
-//     printf("%d\n",sizeof(short*));
-//     printf("%d\n",sizeof(long*));
-//     printf("%d\n",sizeof(long long*));
+//     char arr[10]={0};
+//     int i=0;
+//     for(i=0;i<3;i++)
+//     {
+//         if(i<2)
+//         {
+//             scanf("%c\n",&arr[i]);
+//         }
+//         else
+//         scanf("%c",&arr[i]);
+//     }
+//     for(i=0;i<3;i++)
+//     {
+//         if(arr[i]>='a'&&arr[i]<='z')
+//         {
+//             printf("%c\n",arr[i]-32);
+//         }
+//         if(arr[i]>='A'&&arr[i]<='Z')
+//         {
+//             printf("%c\n",arr[i]+32);
+//         }
+//     }
 //     return 0;
 // }
 // int main()
 // {
-//     int a=10;
-//     *&a=20;
-//     printf("%d\n",a);
+//     char ch=0;
+//     int i=0;
+//     while(scanf("%c",&ch)==1)
+//     {
+//         if(ch>='a'&&ch<='z')
+//         printf("%c\n",ch-32);
+//         else if(ch>='A'&&ch<='Z')
+//         printf("%c\n",ch+32);
+//     }
 //     return 0;
 // }
-struct Stu
-{
-    char name[20];
-    int age;
-    char sex[10];
-};
-void print(struct Stu *p)
-{
-    printf("%s %d %s\n",(*p).name,(*p).age,(*p).sex);
-    printf("%s %d %s\n",p->name,p->age,p->sex);
-}
+// int main()
+// {
+//     char ch=0;
+//     while(1)
+//     {
+//         scanf("%c",&ch);
+//         if(ch>='a'&&ch<='z')
+//         printf("%c\n",ch-32);
+//        else if(ch>='A'&&ch<='Z')
+//         printf("%c\n",ch+32);
+//         getchar();
+//     }
+//     return 0;
+// }
+// #include <ctype.h>
+// int main()
+// {
+//     char ch=0;
+//     while(scanf("%c",&ch) !=EOF)
+//     {
+//         if(isupper(ch))
+//         {
+//             printf("%c\n",tolower(ch));
+//         }
+//         else if(islower(ch))
+//         {
+//             printf("%c\n",toupper(ch));
+//         }
+//     }
+//     return 0;
+// }
+// int main()
+// {
+//     char ch = 0;
+//     while (1)
+//     {
+        
+//            scanf(" %c",&ch);
+           
+//             if (ch >= 'a' && ch <= 'z' || ch >= 'A' && ch <= 'Z')
+//             {
+//                 printf("%c is an Alphabet.\n", ch);
+//             }
+
+//             else
+//             {
+//                 printf("%c is not an Alphabet.\n", ch);
+//             }
+//     }
+
+//     return 0;
+// }
+// int main()
+// {
+//     int i=0;
+//     for(i=10000;i<=99999;i++)
+//     {
+//         int sum=(i/10000)*(i%10000)+(i/1000)*(i%1000)+(i/100)*(i%100)+(i/10)*(i%10);
+//         if(sum==i)
+//         {
+//             printf("%d ",i);
+//         }
+//     }
+//     printf("\n");
+//     return 0;
+// }
+// #include <math.h>
+// int main()
+// {
+//     int i=0;
+//     for(i=10000;i<99999;i++)
+//     {
+//         int j=0;
+//         int sum=0;
+//         for(j=1;j<=4;j++)
+//         {
+//          sum=sum+(i/(int)pow(10,j))*(i%(int)pow(10,j));
+//         }
+//         if(sum==i)
+//         {
+//             printf("%d ",i);
+//         }
+//     }
+//     printf("\n");
+//     return 0;
+// }
 int main()
 {
-    //struct Stu p1={"zhang",18,"man"};
-     struct Stu p2={"wang",19,"woman"};
-    // printf("%s %d %s\n",p1.name,p1.age,p1.sex);
-    //printf("%s %d %s\n",p2.name,p2.age,p2.sex);
-print(&p2);
+    char a=0;
+    char*p=&a;
+    char**pp=&p;
+    printf("%p\n",pp);
+    printf("%p\n",pp+1);
     return 0;
 }
